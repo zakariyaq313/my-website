0ab8c895ec2b20e4351f84357d1d19292ce2af91
Zakariya Qureshi <zakariyaq313@gmail.com>
2020-12-03 20:28:58 +0530

    Carousel/slider updated

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Skill section animation on scroll
-
 gsap.registerPlugin(ScrollTrigger);
 gsap.to("div.skills-container", {
     scrollTrigger: "section.section-skills",
@@ -7,54 +6,18 @@ gsap.to("div.skills-container", {
     opacity: 1
 });
 
+
 // Project carousel
-
-let leftScrollButton = document.querySelector("i.l_scroll");
-let rightScrollButton = document.querySelector("i.r_scroll");
-let projects = document.querySelectorAll("div.project");
-let projectCount = 0;
-
-function scrollReset() {
-    for (let i = 0; i < projects.length; i++) {
-        projects[i].setAttribute("style", "display:none; opacity:0;");
-    }
-}
-
-function scrollRight() {
-    scrollReset();
-    let currentlyVisible = projects[projectCount + 1];
-    currentlyVisible.style.display = "grid";
-    setTimeout( () => {
-        currentlyVisible.style.opacity = "1";
-    }, 100);
-    projectCount++;
-}
-
-function scrollLeft() {
-    scrollReset();
-    let currentlyVisible = projects[projectCount - 1];
-    currentlyVisible.style.display = "grid";
-    setTimeout( () => {
-        currentlyVisible.style.opacity = "1";
-    }, 100);
-    projectCount--;
-}
-
-rightScrollButton.addEventListener("click", () => {
-    if (projectCount === (projects.length - 1)) {
-        projectCount = -1;
-    }
-    scrollRight();
-});
-
-leftScrollButton.addEventListener("click", () => {
-    if (projectCount === 0) {
-        projectCount = projects.length;
-    }
-    scrollLeft();
-});
-
-(function() {
-    scrollReset();
-    projects[0].setAttribute("style", "display:grid; opacity: 1;");
-})();
+let mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+    slideShadows: false,
+    autoplay: { 
+        delay: 4000, 
+    },
+    navigation: {
+      nextEl: '.r_scroll',
+      prevEl: '.l_scroll',
+    },
+})
