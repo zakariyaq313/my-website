738970dd7de2bd5398627d07171798d1e2aae32e
Zakariya Qureshi <zakariyaq313@gmail.com>
2020-12-03 18:28:26 +0530

    Minor fixes

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-// Menu button on mobile devices
+// Skill section animation on scroll
 
-let menuOpen = document.querySelector("i.menu-open");
-let menuClose = document.querySelector("i.menu-close");
-let menuBackground = document.querySelector("span.menu-background");
-let navbar = document.querySelector("nav.navigation");
-
-function loaded() {
-    menuBackground.style.opacity = "1";
-    menuOpen.style.opacity = "1";
-}
-
-window.onload = loaded;
-
-menuOpen.addEventListener("click", () => {
-    menuOpen.style.transform = "rotate(-90deg)";
-    menuBackground.style.transform = "scale(50)";
-    navbar.setAttribute("style", "opacity: 1; right: 20%;");
-
-    setTimeout(() => {
-        menuOpen.style.opacity = "0";
-        menuOpen.style.visibility = "hidden";
-        menuClose.setAttribute("style", "visibility: visible; opacity: 1;");
-    }, 20);
-
-    setTimeout(() => {
-        menuBackground.style.opacity = "0";
-    }, 100);
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("div.skills-container", {
+    scrollTrigger: "section.section-skills",
+    y: 0,
+    opacity: 1
 });
-
-menuClose.addEventListener("click", () => {
-    menuClose.setAttribute("style", "opacity: 0; visibility: hidden;");
-    menuOpen.setAttribute("style", "visibility: visible; opacity: 1; transform: rotate(0);");
-    navbar.setAttribute("style", "right: -30%; opacity: 0;");
-    menuBackground.setAttribute("style", "transform: scale(0); opacity: 1;");
-});
-
-
-// Skill section opacity change on scroll
-
-let skillSection = document.querySelector("div.skills-container");
-
-document.addEventListener("scroll", () => {
-    let skillSectionPosition = skillSection.getBoundingClientRect();
-
-    if (skillSectionPosition.top > innerHeight || skillSectionPosition.bottom < 0) {
-        skillSection.setAttribute("style", "transform: translateY(50rem);")
-    } else {
-        skillSection.setAttribute("style", "transform: translateY(0);")
-    }
-});
-
 
 // Project carousel
 
@@ -63,13 +19,6 @@ function scrollReset() {
         projects[i].setAttribute("style", "display:none; opacity:0;");
     }
 }
-
-function startSlide() {
-    scrollReset();
-    projects[0].setAttribute("style", "display:grid; opacity: 1;");
-}
-
-startSlide();
 
 function scrollRight() {
     scrollReset();
@@ -104,3 +53,8 @@ leftScrollButton.addEventListener("click", () => {
     }
     scrollLeft();
 });
+
+(function() {
+    scrollReset();
+    projects[0].setAttribute("style", "display:grid; opacity: 1;");
+})();
